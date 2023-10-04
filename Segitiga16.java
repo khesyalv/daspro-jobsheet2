cf6b69ebaedf42e71ec5f32c066c36ab3815a16d
khesyalv <145529469+khesyalv@users.noreply.github.com>
2023-10-04 12:44:02 +0700

    Add files via upload

---
+++ Segitiga16.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Segitiga16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int alas, tinggi;
+        float luas;
+
+        System.out.println("Masukkan alas: ");
+        alas = sc.nextInt();
+
+        System.out.println("Masukkan tingi: ");
+        tinggi = sc.nextInt();
+
+        luas = alas * tinggi / 2;
+        System.out.println("Luas segitiga: " + luas);
+    }
+}
